4cdebc85a3c988b5210865ba27228e9b23795eb3
Adam Ansari <adam1ansari1@gmail.com>
2026-09-03 11:33:38 +0100

    Add comment about testing in experiments
    Added a comment indicating that the function will be tested in experiments.

--- kernels.py
+++ kernels.py
@@ -117,3 +117,4 @@ def verify_kernel_trick(X, p=2):
     K_direct = polynomial_kernel(X, X, p=p) #allows us to look at similarity of each Xi event to Xj in same dataset
     max_diff = np.max(np.abs(K_explicit - K_direct))
     return K_explicit, K_direct, max_diff
+#WE TEST THIS IN EXPERIMENTS
